087a3556eb1ada0da7f536e4cb6a44a6133ab683
25000cc <25000ccprogram@gmail.com>
2020-05-10 12:18:28 +0000

    RGBまで実装

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 $(function() {
   'use strict';
   let canvas = $('#canvas')[0];
+  let R = $('#R-range')[0];
+  let G = $('#G-range')[0];
+  let B = $('#B-range')[0];
+  let preview = $('#preview')[0];
 
   let ctx = canvas.getContext('2d');
   let width = canvas.width;
-  let canvasSize = 16;
-  let dotSize = width / canvasSize;
+  let pxSize = 16;
+  let dotSize = width / pxSize;
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, width, width);
+  ctx.fillStyle = 'black';
 
   // グリッド描写
-  for (let i = 1; i < canvasSize; i++) {
-    // ctx.strokeRect(i * dotSize, 0, dotSize, width);
+  for (let i = 1; i < pxSize; i++) {
     ctx.beginPath();
     let xy = i * dotSize
     ctx.moveTo(xy, 0);
@@ -21,6 +28,7 @@ $(function() {
     ctx.stroke();
   }
 
+  // 色塗り処理
   canvas.addEventListener("click", function(event) {
     var clickX = event.pageX ;
     var clickY = event.pageY ;
@@ -33,7 +41,16 @@ $(function() {
     // 要素内におけるクリック位置を計算
     var x = clickX - positionX ;
     var y = clickY - positionY ;
-    console.log('x: %d', x);
-    console.log('y: %d', y);
+    ctx.fillStyle = preview.style.backgroundColor;
+    ctx.fillRect(x - x % dotSize, y - y % dotSize, dotSize, dotSize);
   });
+
+  var rangeValue = function (elem, target) {
+    return function(evt) {
+      target.style.backgroundColor = `rgb(${R.value}, ${G.value}, ${B.value})`;
+    }
+  }
+  R.addEventListener('input', rangeValue(R, preview));
+  G.addEventListener('input', rangeValue(G, preview));
+  B.addEventListener('input', rangeValue(B, preview));
 });
