2e1f32d934a129141495317b4b167d1ecad484b2
25000cc <25000ccprogram@gmail.com>
2020-05-15 14:45:32 +0000

    スポイトを実装

--- script.js
+++ script.js
@@ -6,13 +6,10 @@ $(function() {
   const G = $('#G-range')[0];
   const B = $('#B-range')[0];
   const preview = $('#preview')[0];
-  const pencil = $('.fa-pencil-alt')[0];
-  const drip = $('.fa-fill-drip')[0];
   const ctx = canvas.getContext('2d');
   const width = canvas.width;
   const pxSize = 16;
   const dotSize = width / pxSize;
-  let isDrip = false;
 
   ctx.fillStyle = 'white';
   ctx.fillRect(0, 0, width, width);
@@ -32,6 +29,12 @@ $(function() {
     ctx2.stroke();
   }
 
+  const pencil = $('.fa-pencil-alt')[0];
+  const drip = $('.fa-fill-drip')[0];
+  const dropper = $('.fa-eye-dropper')[0];
+  let isDrip = false;
+  let isDropper = false;
+
   pencil.onclick = () => {
     drip.style.display = 'block';
     pencil.style.display = 'none';
@@ -44,42 +47,79 @@ $(function() {
     isDrip = false;
   }
 
+  dropper.onclick = () => {
+    if (!isDropper) {
+      dropper.style.marginTop = '5px';
+      dropper.style.color = 'gray';
+      isDropper = true;
+    } else {
+      cancelDropper()
+    }
+  }
+
+  function cancelDropper() {
+    dropper.style.marginTop = '0px';
+    dropper.style.color = 'black';
+    isDropper = false;
+  }
+
   // 色塗り処理
   canvas.addEventListener("click", function(event) {
     const rect = event.target.getBoundingClientRect()
     let x = Math.round(event.clientX - rect.left);
     let y = Math.round(event.clientY - rect.top);
-    if (!isDrip) { // 1マス塗り
+    if (isDropper) { // 塗りつぶし（バケツ）
+      dropper_(x, y);
+    } else if (isDrip) { // スポイト
+      drip_(x, y);
+    } else { // 1マス塗り
       ctx.fillStyle = preview.style.backgroundColor;
       ctx.fillRect(x - x % dotSize, y - y % dotSize, dotSize, dotSize);
-    } else { // 塗りつぶし（バケツ）
-      let matrix = [];
-      const data = ctx.getImageData(0, 0, width, width).data;
-      for(let i = 0; i < Math.ceil(data.length / 4); i++) {
-        const start = i * 4;
-        const point = data.slice(start, start + 4);
-        matrix.push(`${point[0]},${point[1]},${point[2]},${point[3]}`);
-      }
-      matrix = chunk(matrix, width);
-
-      const floodFillCanvas = new FloodFillCanvas(matrix);
-      floodFillCanvas.replaceColor = `${R.value},${G.value},${B.value},255`;
-      const table = floodFillCanvas.fill(x, y);
-      const imageData = ctx.getImageData(0, 0, width, width);
-      table.forEach((row, y) => {
-        row.forEach((col, x) => {
-          const pos = floodFillCanvas.startPosition(x, y);
-          if (table[y][x] === `${R.value},${G.value},${B.value},255`) {
-            imageData.data[pos] = R.value;
-            imageData.data[pos + 1] = G.value;
-            imageData.data[pos + 2] = B.value;
-            imageData.data[pos + 3] = 255;
-          }
-        })
-      })
-      ctx.putImageData(imageData, 0, 0)
     }
   });
+
+  function drip_(x, y) {
+    let matrix = [];
+    const data = ctx.getImageData(0, 0, width, width).data;
+    for(let i = 0; i < Math.ceil(data.length / 4); i++) {
+      const start = i * 4;
+      const point = data.slice(start, start + 4);
+      matrix.push(`${point[0]},${point[1]},${point[2]},${point[3]}`);
+    }
+    matrix = chunk(matrix, width);
+    const floodFillCanvas = new FloodFillCanvas(matrix);
+    floodFillCanvas.replaceColor = `${R.value},${G.value},${B.value},255`;
+    const table = floodFillCanvas.fill(x, y);
+    const imageData = ctx.getImageData(0, 0, width, width);
+    table.forEach((row, y) => {
+      row.forEach((col, x) => {
+        const pos = floodFillCanvas.startPosition(x, y);
+        if (table[y][x] === `${R.value},${G.value},${B.value},255`) {
+          imageData.data[pos] = R.value;
+          imageData.data[pos + 1] = G.value;
+          imageData.data[pos + 2] = B.value;
+          imageData.data[pos + 3] = 255;
+        }
+      })
+    })
+    ctx.putImageData(imageData, 0, 0)
+  }
+
+  function setColor(r, g, b) {
+    preview.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    R.value = r;
+    G.value = g;
+    B.value = b;
+  }
+
+  function dropper_(x, y) {
+    const imageData = ctx.getImageData(x, y, 1, 1);
+    const r = imageData.data[0];
+    const g = imageData.data[1];
+    const b = imageData.data[2];
+    setColor(r, g, b)
+    cancelDropper();
+  }
 
   // RGB
   function RGBrange(elem, target) {
@@ -105,11 +145,8 @@ $(function() {
     const block = $(`#block${i}`)[0];
     block.onclick = () => {
       const backgroundColor = window.getComputedStyle(block).backgroundColor;
-      preview.style.backgroundColor = backgroundColor;
       const arr = backgroundColor.match(/[0-9]+\.?[0-9]*/g);
-      for (let i = 0; i < 3; i++) {
-        RGB[i].value = arr[i];
-      }
+      setColor(arr[0], arr[1], arr[2])
     }
   }
 });
